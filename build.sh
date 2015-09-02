d75d4e1c30d0e590150b1fa2b7126a2e9da7fe64
António Meireles <antonio.meireles@reformi.st>
2015-09-02 10:59:02 +0100

    making sure we don't get staled stuff after a new build...
    Signed-off-by: António Meireles <antonio.meireles@reformi.st>

--- build.sh
+++ build.sh
@@ -5,19 +5,22 @@ V="blablabla.go"
 
 echo "package main" > ${V}
 echo "var Version = \"${VERSION}\"" >> ${V}
+rm -rf ./Godeps
 godep save ./...
 git status
-godep go build -o coreos-xhyve *.go
+godep go build -o coreos-xhyve ./*.go
+
+rm -rf ./documentation/*
 mkdir -p ./documentation/{man,markdown}
 COREOS_DEBUG=true ./coreos-xhyve utils mkMan
 (pushd ./documentation/man
-    for page in $(ls *.1); do
-        sed -i '/spf13\/cobra$/ d' ${page}
+    for page in *.1; do
+        sed -i '/spf13\/cobra$/ d' "${page}"
     done
 popd
 COREOS_DEBUG=true ./coreos-xhyve utils mkMkdown
 pushd ./documentation/markdown
-    for page in $(ls *.md); do
-        sed -i '/spf13\/cobra/d' ${page}
+    for page in *.md; do
+        sed -i '/spf13\/cobra/d' "${page}"
     done
 popd) >/dev/null
